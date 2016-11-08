1d17f665cf5bbfdfe6945043acd65bf337089812
Quentin Goinaud <armaldio@gmail.com>
2016-11-08 23:26:13 +0100

    -

--- source/package.bat
+++ source/package.bat
@@ -1,1 +1,1 @@
-electron-packager . "electron-prebuilt-1.4.5" --platform="win32" --arch="x64" --version=1.4.5 --out="../"
+electron-packager . "electron-prebuilt-1.4.5" --platform="win32" --arch="x64" --version=1.4.5 --out="../" --overwrite
