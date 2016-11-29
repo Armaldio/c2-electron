50dc57a47bfca20d879952f0edab4f1b6dd140a2
Quentin Goinaud <armaldio@gmail.com>
2016-11-30 00:39:14 +0100

    v0.1.2
    Basic support for Electron config file
    Electron 1.4.8 => 1.4.10

--- source/main.js
+++ source/main.js
@@ -1,64 +1,55 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const electron      = require('electron');
+const app           = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const fs            = require("fs");
+const path          = require("path");
 
-var client = require('electron-connect').client;
+const client = require('electron-connect').client;
 
 const isDev = require('electron-is-dev');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+let config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json")));
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/www/index.html`)
+function createWindow() {
+	mainWindow = new BrowserWindow(
+		{
+			width : config.window.width,
+			height: config.window.height,
+			frame : config.window.frame
+		}
+	);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-  
-  if (isDev) {
-	console.log('Running in development');
-	client.create(mainWindow);
-  } else {
-    console.log('Running in production');
-  }
+	console.log(mainWindow);
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
+	mainWindow.loadURL(`file://${__dirname}/www/index.html`);
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+	if (config.external_tools.show_console_at_start)
+		mainWindow.webContents.openDevTools();
 
-// Quit when all windows are closed.
+	if (isDev) {
+		console.log('Running in development');
+		client.create(mainWindow);
+	} else {
+		console.log('Running in production');
+	}
+
+	mainWindow.on('closed', function () {
+		mainWindow = null
+	})
+};
+
+app.on('ready', createWindow);
+
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+});
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+	if (mainWindow === null) {
+		createWindow()
+	}
+});
